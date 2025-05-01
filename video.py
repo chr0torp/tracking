ed225bdaad70cca6cdd6d265725a46fe9cd71ac1
chr0torp <christian.torp@hotmail.com>
2025-05-01 16:58:40 +0200

    virker

--- video.py
+++ video.py
@@ -1,16 +1,4 @@
 
-from picamzero import Camera
-import os
-
-home_dir = os.environ['HOME']
-cam = Camera()
-
-cam.start_preview()
-cam.record_video(f"{home_dir}/Desktop/new_video.mp4", duration=30)
-cam.stop_preview()
-
-#!/usr/bin/python3
-
 from picamera2 import Picamera2, Preview
 import time
 import os
