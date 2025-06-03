6e4e191674df4f957811af64cb9d9adf59fa6c91
chr0torp <christian.torp@hotmail.com>
2025-06-03 11:43:47 +0200

    pic

--- pic.py
+++ pic.py
@@ -5,7 +5,7 @@ import os
 
 home_dir = os.path.expanduser("~") 
 print("Home directory:", home_dir)
-place = os.path.join(home_dir, "Desktop", "Pictures", "image0.jpg")
+place = os.path.join(home_dir, "Desktop", "Pictures")
 print("place:", place)
 
 os.makedirs(place, exist_ok=True)
@@ -13,7 +13,8 @@ os.makedirs(place, exist_ok=True)
 camera = Picamera2()
 time.sleep(2)
 
-camera.capture_file(place)
+full_path = os.path.join(place, "image.jpg")
+camera.capture_file(full_path)
 
-
+camera.stop()
 print("Done.")
