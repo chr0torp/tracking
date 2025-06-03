230e0316a1147e1d47721d6485fb443c5c3e80f2
chr0torp <christian.torp@hotmail.com>
2025-06-03 11:35:03 +0200

    pic

--- pic.py
+++ pic.py
@@ -2,18 +2,14 @@ from picamera2 import Picamera2
 import time
 import os
 
+
+home_dir = os.path.expanduser("~") 
+place = os.path.join(home_dir, "Desktop", "Pictures", "image0.jpg")
+
 camera = Picamera2()
 time.sleep(2)
 
-place = "/Desktop/Pictures/image0"
+camera.capture_file(place)
 
-camera.capture_file(place + ".jpg")
-
-# if not os.path.exists(place):
-#     number = 0
-#     name = place + str(number) + ".jpg"
-#     while os.path.exists(name):
-#         number += 1
-#         name = place + str(number) + ".jpg"
 
 print("Done.")
